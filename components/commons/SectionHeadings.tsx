8a2a02361333cc3b606ba5191b76a971b054372d
Kingsley <kingsleyakwa@gmail.com>
2024-08-01 21:25:53 +0100

    feat: animate some components

--- components/commons/SectionHeadings.tsx
+++ components/commons/SectionHeadings.tsx
@@ -1,12 +1,34 @@
+"use client"
+import { motion, useInView } from "framer-motion"
+import { useRef } from "react"
+
 type HeadingProps = {
     heading: string,
     subheading: string
 }
+
 export default function SectionHeadings({ heading, subheading }: HeadingProps) {
+    const ref = useRef(null)
+    const inView = useInView(ref, { once: true })
+
     return (
-        <div className="flex flex-col gap-6 lg:max-w-[80%]">
-            <h3 className="instrument-sans text-lg text-sleekWhite/80">{heading}</h3>
-            <p className="text-sleekWhite text-3xl lg:text-4xl">{subheading}</p>
+        <div ref={ref} className="flex flex-col gap-6 lg:max-w-[80%]">
+            <motion.h3
+                initial={{ x: -100, opacity: 0 }}
+                animate={inView ? { x: 0, opacity: 1 } : {}}
+                transition={{ ease: "easeInOut", duration: 1, delay: 0.4 }}
+                className="instrument-sans text-lg text-sleekWhite/80"
+            >
+                {heading}
+            </motion.h3>
+            <motion.p
+                initial={{ x: 100, opacity: 0 }}
+                animate={inView ? { x: 0, opacity: 1 } : {}}
+                transition={{ ease: "easeInOut", duration: 1, delay: 0.4 }}
+                className="text-sleekWhite text-3xl lg:text-4xl"
+            >
+                {subheading}
+            </motion.p>
         </div>
     )
 }
